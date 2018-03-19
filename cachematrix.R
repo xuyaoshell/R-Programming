c8c0f6fc8e9bf00733153bbe9d9a3434d3322bfc
xuyaoshell <36436803+xuyaoshell@users.noreply.github.com>
2018-03-19 22:10:54 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
       inv <- NULL
       set <- function(y) {
@@ -18,7 +13,6 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
